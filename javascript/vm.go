cbd819de9412f0086d6de661d1117760ea0f31e7
Delta <riku@kawaii.sh>
2020-08-24 13:22:10 -0500

    Event loop

--- javascript/vm.go
+++ javascript/vm.go
@@ -6,20 +6,23 @@ import (
 	"github.com/Riku32/Picnic/handler/command"
 	"github.com/Riku32/Picnic/stdlib/logger"
 	"github.com/dop251/goja"
+	"github.com/dop251/goja_nodejs/eventloop"
 	"github.com/dop251/goja_nodejs/require"
 )
 
 type Vm struct {
-	runtime *goja.Runtime
+	runtime *eventloop.EventLoop
 }
 
 func NewVM() Vm {
-	registry := new(require.Registry)
-	vm := goja.New()
+	vm := eventloop.NewEventLoop()
 
-	registry.Enable(vm)
+	require := require.NewRegistry()
 
-	vm.SetFieldNameMapper(goja.TagFieldNameMapper("json", true))
+	vm.Run(func(vm *goja.Runtime) {
+		vm.SetFieldNameMapper(goja.TagFieldNameMapper("json", true))
+		require.Enable(vm)
+	})
 
 	runtime := Vm{
 		runtime: vm,
@@ -31,18 +34,22 @@ func NewVM() Vm {
 }
 
 func (vm Vm) setglobals() {
-	vm.runtime.Set("logger", logger.JSLogger{})
+	vm.SetGlobal("logger", logger.JSLogger{})
 }
 
 // Execute : execute a js script
 func (vm Vm) Execute(command command.Command) {
-	_, err := vm.runtime.RunString(command.Command)
-	if err != nil {
-		logger.Error(fmt.Sprintf("[%s] %s", command.Prop.Name, err.Error()))
-	}
+	vm.runtime.Run(func(vm *goja.Runtime) {
+		_, err := vm.RunString(command.Command)
+		if err != nil {
+			logger.Error(fmt.Sprintf("[%s] %s", command.Prop.Name, err.Error()))
+		}
+	})
 }
 
 // SetGlobal : set a variable in
 func (vm Vm) SetGlobal(key string, value interface{}) {
-	vm.runtime.Set(key, value)
+	vm.runtime.Run(func(vm *goja.Runtime) {
+		vm.Set(key, value)
+	})
 }
